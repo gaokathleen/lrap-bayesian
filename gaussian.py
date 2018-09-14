3bae8f4981c80c47333e7c9a9707c2808a58d8cd
anoodledoodle <kathleen.gao@berkeley.edu>
2018-09-13 20:47:30 -0700

    Updated np.linspace and fixed graph display

--- gaussian.py
+++ gaussian.py
@@ -3,11 +3,16 @@ import math
 import numpy as np
 def gauss(x, mean, variance):
     deviation = math.sqrt(variance)
-    return math.exp(-(x - mean) ** 2 / (2 * (deviation ** 2)))
+    return np.exp(-np.power(x - mean, 2) / (2 * np.power(deviation, 2)))
 
-# points is a list of coordinate pairs. The coordinate pairs are composed (mean, variance)
+# points is a list of coordinate pairs. The coordinate pairs are tuples containing (mean, variance)
 def plot(points):
     for mean, variance in points:
-        plt.plot(gauss(np.linspace(-10, 10, 100), mean, variance))
+        plt.plot(gauss(np.linspace(mean - 3 * math.sqrt(variance), mean + 3 * math.sqrt(variance), 100), mean, variance))
 
+
+plot([(-100, 4), (1000, 2), (6, 6)])
 plt.show()
+
+#import scipy.stats as stats
+#stats.norm.pdf(x, mean, deviation)
